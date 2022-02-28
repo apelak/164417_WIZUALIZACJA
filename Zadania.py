38d3f827891e559fc050f3a956130290e23a1902
apelak <apelak10@wp.pl>
2022-02-28 17:27:06 +0100

    XYZ

--- Zadania.py
+++ Zadania.py
@@ -1,39 +1,45 @@
+import math
+
 # Zadanie 1
 a1 = 4
 a2 = 5
-print(a1," ",a2)
+print(a1, " ", a2)
 b1 = 4.3
 b2 = 2.1
-print(b1," ",b2)
+print(b1, " ", b2)
 c1 = "wizualizacja"
 c2 = "danych"
 print(c1+" "+c2)
 d1 = 2j
 d2 = 2 + 3.8j
-print(d1," ",d2)
-
-#Zadanie 2
-a = input(float("Podaj 1 liczbe: "))
-b = input(float("Podaj 2 liczbę: "))
-c = input("1.Dodawanie 2.Odejmowanie 3.Mnożenie 4.Dzielenie 5.Potęgownie")
-def dodawanie():
+print(d1, " ", d2)
+del a1, a2, b1, b2, c1, c2, d1, d2
+# Zadanie 2
+a = int(input("Podaj a: "))
+b = int(input("Podaj b: "))
+print("1.Dodawanie 2.Odejmowanie 3.Mnożenie 4.Dzielenie 5.Potęgownie: ")
+c = int(input())
+if c == 1:
     print(a+b)
-def odejmowanie():
+elif c == 2:
     print(a-b)
-def mnozenie():
+elif c == 3:
     print(a*b)
-def dzielenie():
+elif c == 4:
     print(a/b)
-def potegowanie():
-    print(a+b)
+elif c == 5:
+    print(pow(a, b))
+else:
+    print("nieprawidlowa wartosc")
+del a, b, c
+# Zadanie 3
 
-switcher = {
-    1:'dodawanie',
-    2:'odejmowanie',
-    3:'mnozenie',
-    4:'dzielenie',
-    5:'potegowanie',
 
-}
+
+
+
+
+
+
 
 
