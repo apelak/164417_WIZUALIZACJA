69e895367e42b4b8ae30222ed3ef4a2496561d2b
apelak <apelak10@wp.pl>
2022-02-28 19:06:15 +0100

    XYZ

--- Zadania.py
+++ Zadania.py
@@ -14,25 +14,29 @@ d1 = 2j
 d2 = 2 + 3.8j
 print(d1, d2)
 del a1, a2, b1, b2, c1, c2, d1, d2
+
 # Zadanie 2
-#a = int(input("Podaj a: "))
-#b = int(input("Podaj b: "))
-#print("1.Dodawanie 2.Odejmowanie 3.Mnożenie 4.Dzielenie 5.Potęgownie: ")
-#c = int(input())
-#if c == 1:
-#    print(a+b)
-#elif c == 2:
-#    print(a-b)
-#elif c == 3:
-#   print(a*b)
-#elif c == 4:
-#    print(a/b)
-#elif c == 5:
-#    print(pow(a, b))
-#else:
-#    print("nieprawidlowa wartosc")
-#del a, b, c
+
+a = int(input("Podaj a: "))
+b = int(input("Podaj b: "))
+print("1.Dodawanie 2.Odejmowanie 3.Mnożenie 4.Dzielenie 5.Potęgownie: ")
+c = int(input())
+if c == 1:
+    print(a+b)
+elif c == 2:
+    print(a-b)
+elif c == 3:
+   print(a*b)
+elif c == 4:
+    print(a/b)
+elif c == 5:
+    print(pow(a, b))
+else:
+    print("nieprawidlowa wartosc")
+del a, b, c
+
 # Zadanie 3
+
 a = 2
 a += 2
 print(a)
