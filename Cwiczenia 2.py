e054f3d1c3ca211c3e1118b5786bf70f722a39a1
apelak <apelak10@wp.pl>
2022-03-02 14:17:34 +0100

    XYZ

---
+++ Cwiczenia 2.py
@@ -0,0 +1,32 @@
+# a = int(input("Podaj a: "))
+# b = int(input("Podaj b: "))
+#
+# if a == b:
+#     print("liczby sa rowne", a)
+# elif a > b:
+#     print("a =", a, "jest wieksze od b =", b)
+# else:
+#     print("b =", b, "jest wieksza od a =", a)
+
+# a = input("Wpisz pierwsza liczbe: ")
+# b = input("Wpisz druga liczbe: ")
+# c = input("Wpisz trzecia liczbe: ")
+# d = input("Wpisz czwarta liczbe: ")
+#
+# if (a > b) & (c > d):
+#     print("a jest wieksze od b i c jest wieksze od d")
+# else:
+#     print("liczba a jest mniejsza o b lub c jest mniejsze od d")
+
+# for x in range(0, 6, 1):
+#     print(x+1)
+
+# lista = [1, 4, 5, 2, 5, 1, 2, 3, 5]
+# for x in lista:
+#     print(x)
+# else:
+#     print("Zostaly wyswietlone wszystkie elementy listy")
+x = 0
+while x <= 10:
+    print(x)
+    x += 1
