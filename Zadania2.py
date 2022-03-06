a68944f0b77b05445ebb2bab76d4c0d7ebfff8fa
apelak <apelak10@wp.pl>
2022-03-06 15:43:37 +0100

    XYZ

---
+++ Zadania2.py
@@ -0,0 +1,53 @@
+import sys as system
+#Zadanie 1
+
+# ulubioneSporty = ['plywanie', 'siatkowka', 'tenis stolowy', 'silownia']
+# print(ulubioneSporty)
+# ulubioneSporty.reverse()
+# print(ulubioneSporty)
+# ulubioneSporty.append('pilka nozna')
+# ulubioneSporty.append('pilka reczna')
+# print(ulubioneSporty)
+
+#Zadanie 2
+
+# slownik = {'tzw': 'tak zwany', 'ww': 'wyzej wymieniony', 'tzn': 'to znaczy', 'itp': 'i tym podobne'}
+
+# #Zadanie 3
+# i = 0
+# slownikGier = {'bezi': 'Gothic', 'liga': 'league of legends', 'isaac': 'binding of isaac', 'ets': 'euro truck simulator'}
+# for x in slownikGier:
+#     print("%s: %s" % (x, slownikGier[x]))
+#     i += 1
+# print(i)
+
+# #Zadanie 4
+# zdanie = input("tutaj wpisz zdanie: ")
+# print(zdanie.count("a"))
+#
+# #Zadanie 5
+# a = system.stdin.readline()
+# b = system.stdin.readline()
+# c = system.stdin.readline()
+# wynik = int(a) ** int(b) + int(c)
+# system.stdout.write(str(wynik))
+#
+# #Zadanie 6
+#
+# a = int(input("Wczytaj pierwsza liczbe: "))
+# b = int(input("Wczytaj druga liczbe: "))
+# c = int(input("Wczytaj trzecia liczbe: "))
+#
+# if a>b & a>c:
+#     print(a ,"Jest najwieksze")
+# elif b>a & b>c:
+#     print(b, "jest najwieksze")
+# else:
+#     print(c, "jest najwieksze")
+
+#zadanie 7
+lista = [3.5, 4, 8.2, 5.6, 9]
+for i in lista:
+    print(i ** 2, "\n")
+
+#zadanie 8
