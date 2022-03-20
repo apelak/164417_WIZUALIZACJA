242614846868db094f257e142db76bd52e174c94
apelak <apelak10@wp.pl>
2022-03-20 17:04:20 +0100

    XYZ

--- Zadania3.py
+++ Zadania3.py
@@ -23,3 +23,35 @@ slownik = {'mąka': 'kg',
 nowa = [key for key, value in slownik.items() if value == 'szt']
 print(nowa)
 
+#Zadanie 4
+
+def czy_prostokatny(a, b, c):
+    if a**2+b**2==c**2:
+        print("Jest prostokatny")
+        return 1
+    else:
+        print("Nie jest prostokatny")
+        return 0
+
+print(czy_prostokatny(3, 4, 5))
+
+#Zadanie 5
+
+def pole_trapezu(a=4, b=5, h=3):
+    return (a+b)*h/2
+
+print(pole_trapezu())
+
+#Zadanie 6
+
+def iloczyn(a1=1, q=4, n=10):
+    wynik = a1
+    kolejny_wyraz = a1
+    for x in range(n):
+        kolejny_wyraz*=q
+        wynik*=kolejny_wyraz
+    return wynik
+print(iloczyn())
+
+#Zadanie 7
+
